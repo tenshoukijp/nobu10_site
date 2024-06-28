7a8f8040e2d26d32ca4b7902ae624030556f5c3b
翔.jp <komiyama_a@hotmail.com>
2024-06-28 23:02:07 +0900

    こみっと

--- contents.php
+++ contents.php
@@ -66,6 +66,9 @@ $content_hash = [
     'nobu_mod_base_mod_javascript_movie' =>
         [ 'html'=>'nobu_mod_base_mod_javascript_movie.html', 'title'=>'蒼天録.mod' , 'dir'=>'JavaScript'],
 
+    'nobu_site_the_profile' =>
+        [ 'html'=>'nobu_site_the_profile.html', 'title'=>'プロフィール' , 'dir'=>'コンタクト']
+
 ];
 
 
