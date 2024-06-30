278badbddafaa6d304a8ad417b846a96a9168ed7
翔.jp <komiyama_a@hotmail.com>
2024-06-30 09:59:48 +0900

    こみっと

--- index_STR.php
+++ index_STR.php
@@ -4,13 +4,13 @@ require( "contents.php" );
 
 $punnyAddress = "xn--rssx72hmcj.jp-mod.net";
 
-$defaultHomePage = "nobu_str_home";
-
-$indexFileName = "index.html";
-
 $strTitle = "蒼天録.jp";
 
 $strDescription = "蒼天録の改造";
+
+$defaultHomePage = "nobu_str_home";
+
+$indexFileName = "index.html";
 
 require( "common.php");
 ?>
