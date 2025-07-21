2068bfee1cd2e21c95b05dfd44cd141cbbac76fa
翔.jp <komiyama_a@hotmail.com>
2025-07-22 07:06:08 +0900

    description対応や、bootstrap4→5の修正もれ

--- thumbnail_upload.bat
+++ thumbnail_upload.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
 
-cmd /C C:/usr/nextftp/NEXTFTP_CLI.EXE $Host19 -local="G:/repogitory_jp/nobu10_site" -dir="public_html/xn--rssx72hmcj.jp-mod.net/thumbnail" -quit -nosound -minimize -upload=%1 -nokakunin
+cmd /C C:/usr/nextftp/NEXTFTP_CLI_NO_FOCUS.EXE $Host19 -local="G:/repogitory_jp/nobu10_site" -dir="public_html/xn--rssx72hmcj.jp-mod.net/thumbnail" -quit2 -nosound -minimize -upload=%1 -nokakunin
